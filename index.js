83f8ffea335a6b32a87a5304a37434aae2cabee8
Kenny Wong <sk_kenny@msn.com>
2018-03-10 18:06:09 +0800

    3

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(bodyParser.urlencoded({
 
 //This is the route the API will call
 app.post('/new-message', function(req, res) {
-  const message = req.body
+  const message = req.body;
 
   //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
 
   if (!message || message.text.toLowerCase().indexOf('marco') <0) {
     // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
-    return res.end()
+    return res.end('--');
   }
 
   // If we've gotten this far, it means that we have received a message containing the word "marco".
